b2bb7b3dc0025727a0e00100da8ac18e6164eb52
OrlandoOom <lennard@ricken.hamburg>
2021-07-08 17:52:22 +0200

    [TimeConverter] assign timezone as role & timezone constants

--- constants.py
+++ constants.py
@@ -1,12 +1,26 @@
-
-
 class Tokens(object):
-    DISCORD_TOKEN="your token here"
+    DISCORD_TOKEN="insert your token here"
 
 
 class MessageIDs(object):
     RULES_MSGID=854535314436521984
 
-# class Timezones(object):
+class Timezones(object):
+    identifier = {
+        "EST": "Eastern Standard Time",
+        "PST": "Pacific Standard Time",
+        "CST":	"Central Standard Time",
+        "CET": "Central European Time",
+        "CAT": "Central Africa Time",
+        "AEST": "Australian Eastern Standard Time",
+        "AWST": "Australian Western Standard Time",
+        "AKST": "Alaska Standard Time",
+        "CST": "China Standard Time",
+        "KST": "Korea Standard Time",
+        "GMT": "Greenwich Mean Time",
+        "Q":"Quebec Time Zone"
+    }
+
+
 
 # class Channels(object):
